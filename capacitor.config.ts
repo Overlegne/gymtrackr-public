818b0a02d682f23e6e6ce72b7cc1ff31f858d71a
Sjoerd Luyten <sjoerdluyten@gmail.com>
2026-06-02 11:14:24 +0000

    Revert the static export approach for this Next.js app.
    Reason:
    This ap

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.overlegne.gymtrackr',
+  appName: 'gymtrackr',
+  webDir: 'public'
+};
+
+export default config;
